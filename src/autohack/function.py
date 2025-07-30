6110456f9bdf79536fee09168d165147507589cc
Gavin <sganyunchi@163.com>
2025-07-30 23:11:11 +0800

    chore: 为函数添加返回类型注解

--- src/autohack/function.py
+++ src/autohack/function.py
@@ -2,7 +2,7 @@ from . import exception
 import subprocess
 
 
-def compileCode(compileCommand: str, fileName: str):
+def compileCode(compileCommand: str, fileName: str) -> None:
     print(f"\x1b[1K\rCompile {fileName}.", end="")
     process = subprocess.Popen(
         compileCommand, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True
