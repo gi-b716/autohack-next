0050b860e71d616ffad52db643409c4bf7471cba
Gavin <sganyunchi@163.com>
2025-09-09 19:04:54 +0800

    refactor: 使用 dict 而非 Dict

--- src/autohack/core/constant.py
+++ src/autohack/core/constant.py
@@ -1,4 +1,4 @@
-from typing import Dict, Any, Literal
+from typing import Any
 
 VERSION = "1.0.9"
 
@@ -9,7 +9,7 @@ WAIT_TIME_BEFORE_START = 3
 
 DATA_FOLDER_MAX_SIZE = 256 * 1024 * 1024  # 256 MiB
 
-DEFAULT_CONFIG: Dict[str, Any] = {
+DEFAULT_CONFIG: dict[str, Any] = {
     "_version": 9,
     "refresh_speed": 10,
     "maximum_number_of_data": 0,
