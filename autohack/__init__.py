dc28877a4a4de28b87a002acd6e7aba544704294
Gavin <sganyunchi@163.com>
2026-01-25 23:14:21 +0800

    refactor: AppCentral

--- autohack/__init__.py
+++ autohack/__init__.py
@@ -1,1 +1,23 @@
 __VERSION__ = "1.1.0.dev5"
+
+
+def entrypoint():
+    from argparse import ArgumentParser
+    import uuid, time
+    import colorama
+    from autohack.central import AppCentral
+
+    colorama.just_fix_windows_console()
+
+    argsParser = ArgumentParser(
+        prog="autohack",
+        description="autohack-next - Automated hack data generator",
+    )
+    argsParser.add_argument("--version", "-v", action="version", version=__VERSION__)
+
+    args = argsParser.parse_args()
+
+    app = AppCentral(str(uuid.uuid4()), time.localtime(), debug=False)
+    app.run()
+
+    # TODO: Keyboard interrupt handling
--- autohack/scripts.py
+++
@@ -1,5 +0,0 @@
-def entrypoint() -> None:
-    import os
-
-    os.environ["AUTOHACK_ENTRYPOINT"] = "1"
-    from autohack import __main__
